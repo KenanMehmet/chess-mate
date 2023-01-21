7081572d7b0450bc152047aa3ec8654bbdc5d20e
KenanMehmet <kenmehmet1@gmail.com>
2023-01-21 15:02:24 +0000

    created the sign up page and dashboard page, form works, added authenctation decorator

--- chess/urls.py
+++ chess/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import ChessIndex
+from .views import ChessIndex, ChessPlayerSignup, ChessPlayerDashboard
+from .authenticaion import user_required
 
 urlpatterns = [
     path('', ChessIndex.as_view(), name="index"),
+    path('signup', ChessPlayerSignup.as_view(), name="signup"),
+    path('dashboard', user_required(ChessPlayerDashboard.as_view()), name="dashboard")
 ]
